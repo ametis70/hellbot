527bc40170d00ccea59802f9811141c25879783f
Ian Mancini <ianmethyst@gmail.com>
2021-12-13 15:50:36 -0300

    feat: Add `sqlx` and tag data structs

--- data.go
+++ data.go
@@ -9,64 +9,68 @@ import (
 )
 
 type FactionStatus struct {
-	Season            int    `json:"season"`
-	Points            int    `json:"points"`
-	PointsTaken       int    `json:"points_taken"`
-	PointsMax         int    `json:"points_max"`
-	Status            string `json:"status"`
-	IntroductionOrder int    `json:"introduction_order"`
+	Season            int    `json:"season" db:"season"`
+	Points            int    `json:"points" db:"points"`
+	PointsTaken       int    `json:"points_taken" db:"points_taken"`
+	PointsMax         int    `json:"points_max" db:"points_max"`
+	Status            string `json:"status" db:"status"`
+	IntroductionOrder int    `json:"introduction_order" db:"introduction_order"`
 }
 
 type Event struct {
-	Season    int    `json:"season"`
-	ID        int    `json:"event_id"`
-	StartTime int    `json:"start_time"`
-	EndTime   int    `json:"end_time"`
-	Enemy     int    `json:"enemy"`
-	PointsMax int    `json:"points_max"`
-	Points    int    `json:"points"`
-	Status    string `json:"status"`
+	Season    int    `json:"season" db:"season"`
+	ID        int    `json:"event_id" db:"event_id"`
+	StartTime int    `json:"start_time" db:"start_time"`
+	EndTime   int    `json:"end_time" db:"end_time"`
+	Enemy     int    `json:"enemy" db:"enemy"`
+	PointsMax int    `json:"points_max" db:"points_max"`
+	Points    int    `json:"points" db:"points"`
+	Status    string `json:"status" db:"status"`
 }
 
 type DefendEvent struct {
 	Event
-	Region int `json:"region"`
+	Region int `json:"region" db:"region"`
 }
 
 type AttackEvent struct {
 	Event
-	PlayersAtStart int `json:"players_at_start"`
-	MaxEventId     int `json:"max_event_id"`
+	PlayersAtStart int `json:"players_at_start" db:"players_at_start"`
+	MaxEventId     int `json:"max_event_id" db:"max_event_id"`
 }
 
 type Statistics struct {
-	Season                 int `json:"season"`
-	SeasonDuration         int `json:"season_duration"`
-	Enemy                  int `json:"enemy"`
-	Players                int `json:"players"`
-	TotalUniquePlayers     int `json:"total_unique_players"`
-	Missions               int `json:"missions"`
-	SuccessfulMissions     int `json:"successful_missions"`
-	TotalMissionDifficulty int `json:"total_mission_difficulty"`
-	CompletedPlanets       int `json:"completed_planets"`
-	DefendEvents           int `json:"defend_events"`
-	SuccessfulDefendEvents int `json:"successful_defend_events"`
-	AttackEvents           int `json:"attack_events"`
-	SuccessfulAttackEvents int `json:"successful_attack_events"`
-	Deaths                 int `json:"deaths"`
-	Kills                  int `json:"kills"`
-	Accidentals            int `json:"accidentals"`
-	Shots                  int `json:"shots"`
-	Hits                   int `json:"hits"`
+	Season                 int `json:"season" db:"season"`
+	SeasonDuration         int `json:"season_duration" db:"season_duration"`
+	Enemy                  int `json:"enemy" db:"enemy"`
+	Players                int `json:"players" db:"players"`
+	TotalUniquePlayers     int `json:"total_unique_players" db:"total_unique_players"`
+	Missions               int `json:"missions" db:"missions"`
+	SuccessfulMissions     int `json:"successful_missions" db:"successful_missions"`
+	TotalMissionDifficulty int `json:"total_mission_difficulty" db:"total_mission_difficulty"`
+	CompletedPlanets       int `json:"completed_planets" db:"completed_planets"`
+	DefendEvents           int `json:"defend_events" db:"defend_events"`
+	SuccessfulDefendEvents int `json:"successful_defend_events" db:"successful_defend_events"`
+	AttackEvents           int `json:"attack_events" db:"attack_events"`
+	SuccessfulAttackEvents int `json:"successful_attack_events" db:"successful_attack_events"`
+	Deaths                 int `json:"deaths" db:"deaths"`
+	Kills                  int `json:"kills" db:"kills"`
+	Accidentals            int `json:"accidentals" db:"accidentals"`
+	Shots                  int `json:"shots" db:"shots"`
+	Hits                   int `json:"hits" db:"hits"`
+}
+
+type CampaignStatus struct {
+	Time  int `json:"time" db:"time"`
+	Error int `json:"error_code" db:"error_code"`
 }
 
 type Data struct {
-	Time          int              `json:"time"`
-	Error         int              `json:"error_code"`
-	FactionStatus [3]FactionStatus `json:"campaign_status"`
-	DefendEvent   DefendEvent      `json:"defend_event"`
-	AttackEvents  [3]AttackEvent   `json:"attack_events"`
-	Statistics    [3]Statistics    `json:"statistics"`
+	CampaignStatus
+	FactionStatus [3]FactionStatus `json:"campaign_status" db:"campaign_status"`
+	DefendEvent   DefendEvent      `json:"defend_event" db:"defend_event"`
+	AttackEvents  [3]AttackEvent   `json:"attack_events" db:"attack_events"`
+	Statistics    [3]Statistics    `json:"statistics" db:"statistics"`
 }
 
 func FetchData() (*Data, error) {
--- db.go
+++ db.go
@@ -2,29 +2,19 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
 	"html/template"
 	"log"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	db *sql.DB
+	db *sqlx.DB
 )
 
 func OpenDatabase() {
-	var err error
-
-	db, err = sql.Open("sqlite3", "file:db.sqlite?fk=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	db = sqlx.MustConnect("sqlite3", "file:db.sqlite?fk=true")
 }
 
 func InitDatabase() {
@@ -91,58 +81,34 @@ CREATE TABLE IF NOT EXISTS statistics (
 	var sqlStmt bytes.Buffer
 	t := template.Must(template.New("s").Parse(s))
 	t.Execute(&sqlStmt, map[string]interface{}{"fk": foreignKeySchema, "event": eventSchema})
-	_, err := db.Exec(sqlStmt.String(), foreignKeySchema, eventSchema)
-
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
+	db.MustExec(sqlStmt.String(), foreignKeySchema, eventSchema)
 }
 
 func StoreData(data *Data) {
-	tx, err := db.Begin()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, campaignStatusErr := tx.Exec("INSERT INTO campaign_status (time, error) VALUES (?, ?)", data.Time, data.Error)
-
-	if campaignStatusErr != nil {
-		_ = tx.Rollback()
-		log.Fatal(campaignStatusErr)
-	}
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO campaign_status (time, error) VALUES (?, ?)", data.Time, data.Error)
 
 	for _, faction := range data.FactionStatus {
-		_, factionStatusErr := tx.Exec(
+		tx.MustExec(
 			`INSERT INTO faction_status 
-            (season, points, points_taken, points_max, status, time)
-            VALUES (?, ?, ?, ?, ?, ?)`,
-			faction.Season, faction.Points, faction.PointsTaken, faction.PointsMax, faction.Status, data.Time)
-
-		if factionStatusErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(factionStatusErr)
-		}
+            (season, points, points_taken, points_max, status, introduction_order, time)
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+			faction.Season, faction.Points, faction.PointsTaken, faction.PointsMax, faction.Status,
+			faction.IntroductionOrder, data.Time)
 	}
 
 	for _, attack := range data.AttackEvents {
-		_, attackEventErr := tx.Exec(
+		tx.MustExec(
 			`INSERT INTO attack_events
             (season, event_id, start_time, end_time, enemy, points_max, points, status, players_at_start, max_event_id, time)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
 			attack.Season, attack.ID, attack.StartTime, attack.EndTime, attack.Enemy, attack.PointsMax,
 			attack.Points, attack.Status, attack.PlayersAtStart, attack.MaxEventId, data.Time,
 		)
-
-		if attackEventErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(attackEventErr)
-		}
 	}
 
 	defend := data.DefendEvent
-	_, defendEventErr := tx.Exec(
+	tx.MustExec(
 		`INSERT INTO defend_events 
             (season, event_id, start_time, end_time, enemy, points_max, points, status, region, time)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
@@ -150,26 +116,17 @@ func StoreData(data *Data) {
 		defend.Points, defend.Status, defend.Region, data.Time,
 	)
 
-	if defendEventErr != nil {
-		_ = tx.Rollback()
-		log.Fatal(defendEventErr)
-	}
-
 	for _, statistics := range data.Statistics {
-		_, statisticsErr := tx.Exec(
+		tx.MustExec(
 			`INSERT INTO statistics 
-    (season, season_duration, enemy, players, total_unique_players, missions, successful_missions, total_mission_difficulty, completed_planets, defend_events, successful_defend_events, attack_events, successful_attack_events, deaths, kills, accidentals, shots, hits, time)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            (season, season_duration, enemy, players, total_unique_players, missions, successful_missions, total_mission_difficulty,
+            completed_planets, defend_events, successful_defend_events, attack_events, successful_attack_events, deaths, kills,
+            accidentals, shots, hits, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
 			statistics.Season, statistics.SeasonDuration, statistics.Enemy, statistics.Players, statistics.TotalUniquePlayers,
 			statistics.Missions, statistics.SuccessfulMissions, statistics.TotalMissionDifficulty, statistics.CompletedPlanets,
 			statistics.DefendEvents, statistics.SuccessfulDefendEvents, statistics.AttackEvents, statistics.SuccessfulAttackEvents,
 			statistics.Deaths, statistics.Kills, statistics.Accidentals, statistics.Shots, statistics.Hits, data.Time,
 		)
-
-		if statisticsErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(statisticsErr)
-		}
 	}
 
 	if err := tx.Commit(); err != nil {
