8fe3cd19d5baf39e7c2a4ab72ddcd04c8cdb133e
Ian Mancini <ianmethyst@gmail.com>
2021-12-27 03:28:02 -0300

    fix: Fix and refactor (defend) events handling

--- main.go
+++ main.go
@@ -25,45 +25,8 @@ func loop() {
 		return
 	}
 
-	pData, err := GetLatestData()
 	StoreData(data)
-
-	if err != nil {
-		log.Println("There's no previous data to compare, skipping iteration")
-		return
-	}
-
-	channel := os.Getenv("CHANNEL_ID")
-
-	if (pData.DefendEvent.Status != data.DefendEvent.Status) ||
-		(pData.DefendEvent.ID != data.DefendEvent.ID) {
-		p := pData.DefendEvent
-		n := data.DefendEvent
-
-		switch n.Status {
-		case "active":
-			if p.Status == "failure" || p.Status == "success" || p.ID != p.ID {
-				msg := fmt.Sprintf("New defend event against %v in region %v\nStart Time: %v\nEnd time: %v\nID: %v",
-					n.Enemy, n.Region, time.Unix(int64(n.StartTime), 0).UTC(), time.Unix(int64(n.EndTime), 0).UTC(), n.ID)
-				dg.ChannelMessageSend(channel, msg)
-			}
-
-		case "failure":
-			if p.Status == "active" && p.ID == n.ID {
-				msg := fmt.Sprintf("We failed! the %v have taken back region %v\nID: %v",
-					n.Enemy, n.Region, n.ID)
-				dg.ChannelMessageSend(channel, msg)
-			}
-
-		case "success":
-			if p.Status == "active" && p.ID == n.ID {
-				msg := fmt.Sprintf("We did it! Super Earth has conquered region %v against %v\nID: %v",
-					n.Region, n.Enemy, n.ID)
-				dg.ChannelMessageSend(channel, msg)
-			}
-
-		}
-	}
+	HandleEvents(data)
 }
 
 func main() {
